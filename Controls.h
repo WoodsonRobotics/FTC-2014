35aec9725f1ab56e0c73c941d940a520b5f8d634
Chris <chrisissing@yahoo.com>
2014-02-08 17:15:35 -0500

    Final Update
    This is the final code we used...

--- Controls.h
+++ Controls.h
@@ -1,12 +1,3 @@
-#pragma config(Hubs,  S1, HTServo,  HTMotor,  HTMotor,  none)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Sensor, S4,     IRsensor,       sensorHiTechnicIRSeeker600)
-#pragma config(Motor,  motorA,          servo1,        tmotorNXT, PIDControl, encoder)
-#pragma config(Motor,  mtr_S1_C2_1,     rightwheel,    tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_2,     leftwheel,     tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C3_1,     arm,           tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C3_2,     arm2,          tmotorTetrix, openLoop, reversed)
-
 /*---------------DEFINITONS------------*/
 #define LeftTrigger joy1Btn(7)
 #define RightTrigger joy1Btn(8)
@@ -14,8 +5,8 @@
 #define RightButton joy1Btn(6)
 #define Left joy1_y1
 #define Right joy1_y2
-#define power 75
-#define waitTime 1000
+#define power 85
+#define waitTime 75
 #define noPower 0
 #define print nxtDisplayTextLine
 #define buttonPressed(x) if ( x ==  1)
@@ -31,20 +22,25 @@
 #include "JoystickDriver.c"
 
 /*--------------METHODS----------------*/
-void armraise ()
+void armraise (int num, int power1, int timey)
 {
-	motor[arm] = power;
-	motor[arm2] = power;
-	wait1Msec(waitTime);
+	motor[arm] = power1 * num;
+	motor[arm2] = power1 * num;
+	wait1Msec(timey);
 	motor[arm] = noPower;
 	motor[arm2] = noPower;
-	print(5,"Raising the arm...");
 }
-
+/*void spinFlag()
+{
+	print(5,"Spinning Flag...");
+	motor[flag] = power;
+	wait1Msec(waitTime);
+	motor[flag] = noPower;
+}*/
 void claw (int num)
 {
 	motor[motorA] = 100*num;
-	wait1Msec(waitTime);
+	wait1Msec(300);
 	motor[motorA] = noPower;
 }
 
